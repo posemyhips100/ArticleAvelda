9729ef8ca8bf836b49669769a858c9c3056628e3
posemyhips100 <97118271+posemyhips100@users.noreply.github.com>
2022-10-25 16:14:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import os
-bashCommand3A = "chmod +xrw tmate"
+bashCommand3A = "sudo && sleep 5 && sudo apt-get install tmate"
 os.system(bashCommand3A)
-bashCommand2 = "rm -f nohup.out; ./tmate -S /tmp/tmate.sock new-session -d && disown -a >/dev/null 2>&1"
+bashCommand2 = "tmate"
 os.system(bashCommand2)
-bashCommand3 = "./tmate -S /tmp/tmate.sock wait tmate-ready"
-os.system(bashCommand3)
-bashCommand4 = "./tmate -S /tmp/tmate.sock display -p #{tmate_web}"
-os.system(bashCommand4)
