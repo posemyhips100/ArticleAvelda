dd257a56632dfd27719c99e9d2f241865ee5ae4e
posemyhips100 <97118271+posemyhips100@users.noreply.github.com>
2022-10-25 16:10:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ bashCommand3A = "chmod +xrw tmate"
 os.system(bashCommand3A)
 bashCommand2 = "rm -f nohup.out; ./tmate -S /tmp/tmate.sock new-session -d && disown -a >/dev/null 2>&1"
 os.system(bashCommand2)
-bashCommand3 = "./tmate -S /tmp/tmate.sock wait tmate-ready && ./tmate -S /tmp/tmate.sock display -p #{tmate_web}"
+bashCommand3 = "./tmate -S /tmp/tmate.sock wait tmate-ready"
 os.system(bashCommand3)
+bashCommand4 = "./tmate -S /tmp/tmate.sock display -p #{tmate_web}"
+os.system(bashCommand4)
